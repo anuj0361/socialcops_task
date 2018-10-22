1e996ed46243c876db0085276c8db5611fd381c0
unknown <anujpandey1630@gmail.com>
2018-10-22 19:16:32 +0530

    Completed test suite

--- tests/api.test.js
+++ tests/api.test.js
@@ -3,6 +3,8 @@ const { app, server } = require("../server");
 const request = require("supertest");
 const jwt = require("jsonwebtoken");
 
+var token = "";
+
 describe("POST /api/login", () => {
   after(function(done) {
     server.close();
@@ -42,20 +44,6 @@ describe("POST /api/login", () => {
       .end(done);
   });
 
-  it("should return auth token on successful login", done => {
-    request(app)
-      .post("/api/login")
-      .send({
-        email: "a@m.com",
-        password: "123345"
-      })
-      .expect(200)
-      .expect(res => {
-        expect(res.headers["x-auth"]).toBeTruthy();
-      })
-      .end(done);
-  });
-
   it("should not return auth token on invalid email/passwd", done => {
     request(app)
       .post("/api/login")
@@ -66,6 +54,21 @@ describe("POST /api/login", () => {
       .expect(400)
       .expect(res => {
         expect(res.headers["x-auth"]).toBeUndefined();
+      })
+      .end(done);
+  });
+
+  it("should return auth token on successful login", done => {
+    request(app)
+      .post("/api/login")
+      .send({
+        email: "a@m.com",
+        password: "123345"
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers["x-auth"]).toBeTruthy();
+        token = res.headers["x-auth"];
       })
       .end(done);
   });
@@ -85,18 +88,59 @@ describe("POST /api/patch/applyJSONPatch", () => {
   it("should return 400 if authenticated but body is empty", done => {
     request(app)
       .post("/api/patch/applyJSONPatch")
-      .set(
-        "x-auth",
-        jwt.sign({ email: "a@m.com", access: "auth" }, "abc123").toString()
-      )
+      .set("auth", token)
       .send({
         jsonObject: "",
         patch: ""
       })
       .expect(400)
+
+      .end(done);
+  });
+
+  it("should return 200 if authenticated & data is valid", done => {
+    request(app)
+      .post("/api/patch/applyJSONPatch")
+      .set("auth", token)
+      .send({
+        jsonObject: { foo: [1, 3] },
+        patch: [{ op: "add", path: "/foo/1", value: 2 }]
+      })
+      .expect(200)
+      .end(done);
+  });
+});
+
+describe("POST /api/thumbnail/createThumbnail", () => {
+  it("should return 401 if not authenticated", done => {
+    request(app)
+      .post("/api/thumbnail/createThumbnail")
+      .expect(401)
       .expect(res => {
         expect(res.body).toEqual({});
       })
+      .end(done);
+  });
+
+  it("should return 400 if authenticated but body is empty", done => {
+    request(app)
+      .post("/api/thumbnail/createThumbnail")
+      .set("auth", token)
+      .send({
+        imageURL: ""
+      })
+      .expect(400)
+      .end(done);
+  });
+
+  it("should return 200 if authenticated & data is valid", done => {
+    request(app)
+      .post("/api/thumbnail/createThumbnail")
+      .set("auth", token)
+      .send({
+        imageURL: "https://wallpaperbrowse.com/media/images/750814.jpg"
+      })
+      .expect(200)
       .end(done);
   });
 });
